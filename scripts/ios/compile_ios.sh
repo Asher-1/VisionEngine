1259723d328337f7fec228aee1a8df63078e39ee
dahailu <ludahai19@163.com>
2021-07-29 19:46:11 +0800

    fix ios compiling issues

--- scripts/ios/compile_ios.sh
+++ scripts/ios/compile_ios.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-BUILD_DIR=build-ios
-mkdir -p $BUILD_DIR
-cd $BUILD_DIR
-
-if [ $# != 8 ]; then
+if [ $# != 9 ]; then
   NCNN_VULKAN=ON
   WITH_FULL_OPENCV=OFF
   INSTALL_EXAMPLES=OFF
@@ -12,7 +8,15 @@ if [ $# != 8 ]; then
   VULKAN_PATH="/Users/huang/Desktop/ncnn-20210525-full-source/vulkansdk-macos-1.2.162.0"
   NCNN_PATH="/Users/huang/Desktop/Cpp-VisionEigen/lib/ncnn-20210525-ios-vulkan"
   OPENCV_PATH="/Users/huang/Desktop/Cpp-VisionEigen/lib/opencv-mobile-4.5.1-ios"
-  INSTALL_DIR=$(pwd)/visionEngine-ios-vulkan
+  BUILD_DIR=build-ios
+  ROOT_DIR=$(pwd)/visionEngine-ios-vulkan
+  INSTALL_DIR=$ROOT_DIR/visionengine.framework/Versions/A
+  mkdir -p $INSTALL_DIR/Headers
+  mkdir -p $INSTALL_DIR/Resources
+  ln -s A $ROOT_DIR/visionengine.framework/Versions/Current
+  ln -s Versions/Current/Headers $ROOT_DIR/visionengine.framework/Headers
+  ln -s Versions/Current/Resources $ROOT_DIR/visionengine.framework/Resources
+  ln -s Versions/Current/visionengine $ROOT_DIR/visionengine.framework/visionengine
 else
   TOOLCHAIN_PATH="$1"
   VULKAN_PATH="$2"
@@ -21,23 +25,25 @@ else
   NCNN_VULKAN="$5"
   WITH_FULL_OPENCV="$6"
   INSTALL_EXAMPLES="$7"
-  INSTALL_DIR="$8"
+  BUILD_DIR="$8"
+  INSTALL_DIR="$9"
 fi
+
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
 
 echo "NCNN_PATH: $NCNN_PATH"
 echo "OPENCV_PATH: $OPENCV_PATH"
 
 if [ $NCNN_VULKAN == 'ON'  ]; then
 # vulkan is only available on arm64 devices
-cmake -G Xcode \
-      -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_PATH/ios.toolchain.cmake \
+cmake -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_PATH/ios.toolchain.cmake \
       -DCMAKE_BUILD_TYPE="Release" \
       -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
-      -DIOS_PLATFORM=OS64COMBINED  \
-      #-DIOS_PLATFORM=OS64  \
+      -DIOS_PLATFORM=OS64  \
       -DIOS_ARCH="arm64;arm64e" \
       -DMIRROR_BUILD_IOS=ON \
-      -DMIRROR_BUILD_EXAMPLES=ON \
+      -DMIRROR_BUILD_EXAMPLES=OFF \
       -DMIRROR_INSTALL_EXAMPLES=$INSTALL_EXAMPLES \
       -DMIRROR_BUILD_WITH_FULL_OPENCV=$WITH_FULL_OPENCV \
       -DENABLE_BITCODE=0 \
@@ -56,7 +62,7 @@ else
         -DIOS_PLATFORM=OS \
         -DIOS_ARCH="armv7;arm64;arm64e" \
         -DMIRROR_BUILD_IOS=ON \
-        -DMIRROR_BUILD_EXAMPLES=ON \
+        -DMIRROR_BUILD_EXAMPLES=OFF \
         -DMIRROR_INSTALL_EXAMPLES=$INSTALL_EXAMPLES \
         -DMIRROR_BUILD_WITH_FULL_OPENCV=$WITH_FULL_OPENCV \
         -DENABLE_BITCODE=0 \
--- scripts/ios/ios_compile_batch.sh
+++ scripts/ios/ios_compile_batch.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-INSTALL_DIR=$(pwd)/visionEngine-ios-vulkan
-mkdir -p $INSTALL_DIR
+ROOT_DIR=$(pwd)/visionEngine-ios-vulkan
+INSTALL_DIR=$ROOT_DIR/visionengine.framework/Versions/A
+mkdir -p $INSTALL_DIR/Headers
+mkdir -p $INSTALL_DIR/Resources
+ln -s A $ROOT_DIR/visionengine.framework/Versions/Current
+ln -s Versions/Current/Headers $ROOT_DIR/visionengine.framework/Headers
+ln -s Versions/Current/Resources $ROOT_DIR/visionengine.framework/Resources
+ln -s Versions/Current/visionengine $ROOT_DIR/visionengine.framework/visionengine
 
 BUILD_DIR=build-ios
 NCNN_VULKAN=ON
@@ -13,5 +19,5 @@ NCNN_PATH="/Users/huang/Desktop/Cpp-VisionEigen/lib/ncnn-20210525-ios-vulkan"
 OPENCV_PATH="/Users/huang/Desktop/Cpp-VisionEigen/lib/opencv-mobile-4.5.1-ios"
 
 echo 'Build ios!'
-sh compile_ios.sh $TOOLCHAIN_PATH $VULKAN_PATH $NCNN_PATH $OPENCV_PATH $NCNN_VULKAN $WITH_FULL_OPENCV $INSTALL_EXAMPLES $INSTALL_DIR
+sh compile_ios.sh $TOOLCHAIN_PATH $VULKAN_PATH $NCNN_PATH $OPENCV_PATH $NCNN_VULKAN $WITH_FULL_OPENCV $INSTALL_EXAMPLES $BUILD_DIR $INSTALL_DIR
 rm -rf $BUILD_DIR
