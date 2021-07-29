b7edd3f4dd9477390f2905ac80368d3f1f82f882
dahailu <ludahai19@163.com>
2021-07-29 18:19:45 +0800

    fix ios compiling issues

--- scripts/ios/compile_ios.sh
+++ scripts/ios/compile_ios.sh
@@ -4,7 +4,7 @@ BUILD_DIR=build-ios
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 
-if [ $# != 9 ]; then
+if [ $# != 8 ]; then
   NCNN_VULKAN=ON
   WITH_FULL_OPENCV=OFF
   INSTALL_EXAMPLES=OFF
@@ -12,7 +12,6 @@ if [ $# != 9 ]; then
   VULKAN_PATH="/Users/huang/Desktop/ncnn-20210525-full-source/vulkansdk-macos-1.2.162.0"
   NCNN_PATH="/Users/huang/Desktop/Cpp-VisionEigen/lib/ncnn-20210525-ios-vulkan"
   OPENCV_PATH="/Users/huang/Desktop/Cpp-VisionEigen/lib/opencv-mobile-4.5.1-ios"
-  OPENMP_LIBRARY="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/lib/libomp.a"
   INSTALL_DIR=$(pwd)/visionEngine-ios-vulkan
 else
   TOOLCHAIN_PATH="$1"
@@ -22,8 +21,7 @@ else
   NCNN_VULKAN="$5"
   WITH_FULL_OPENCV="$6"
   INSTALL_EXAMPLES="$7"
-  OPENMP_LIBRARY="$8"
-  INSTALL_DIR="$9"
+  INSTALL_DIR="$8"
 fi
 
 echo "NCNN_PATH: $NCNN_PATH"
@@ -31,23 +29,20 @@ echo "OPENCV_PATH: $OPENCV_PATH"
 
 if [ $NCNN_VULKAN == 'ON'  ]; then
 # vulkan is only available on arm64 devices
-cmake -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_PATH/ios.toolchain.cmake \
+cmake -G Xcode \
+      -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_PATH/ios.toolchain.cmake \
       -DCMAKE_BUILD_TYPE="Release" \
       -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
-      -DIOS_PLATFORM=OS64 \
+      -DIOS_PLATFORM=OS64COMBINED  \
+      #-DIOS_PLATFORM=OS64  \
       -DIOS_ARCH="arm64;arm64e" \
       -DMIRROR_BUILD_IOS=ON \
-      -DMIRROR_BUILD_EXAMPLES=OFF \
+      -DMIRROR_BUILD_EXAMPLES=ON \
       -DMIRROR_INSTALL_EXAMPLES=$INSTALL_EXAMPLES \
       -DMIRROR_BUILD_WITH_FULL_OPENCV=$WITH_FULL_OPENCV \
       -DENABLE_BITCODE=0 \
       -DENABLE_ARC=0 \
       -DENABLE_VISIBILITY=0 \
-      -DOpenMP_C_FLAGS="-Xclang -fopenmp" \
-      -DOpenMP_CXX_FLAGS="-Xclang -fopenmp" \
-      -DOpenMP_C_LIB_NAMES="libomp" \
-      -DOpenMP_CXX_LIB_NAMES="libomp" \
-      -DOpenMP_libomp_LIBRARY=$OPENMP_LIBRARY \
       -DOPENCV_PATH=$OPENCV_PATH \
       -DNCNN_PATH=$NCNN_PATH \
       -DVULKAN_PATH=$VULKAN_PATH \
@@ -61,21 +56,16 @@ else
         -DIOS_PLATFORM=OS \
         -DIOS_ARCH="armv7;arm64;arm64e" \
         -DMIRROR_BUILD_IOS=ON \
-        -DMIRROR_BUILD_EXAMPLES=OFF \
+        -DMIRROR_BUILD_EXAMPLES=ON \
         -DMIRROR_INSTALL_EXAMPLES=$INSTALL_EXAMPLES \
         -DMIRROR_BUILD_WITH_FULL_OPENCV=$WITH_FULL_OPENCV \
         -DENABLE_BITCODE=0 \
         -DENABLE_ARC=0 \
         -DENABLE_VISIBILITY=0 \
-        -DOpenMP_C_FLAGS="-Xclang -fopenmp" \
-        -DOpenMP_CXX_FLAGS="-Xclang -fopenmp" \
-        -DOpenMP_C_LIB_NAMES="libomp" \
-        -DOpenMP_CXX_LIB_NAMES="libomp" \
-        -DOpenMP_libomp_LIBRARY=$OPENMP_LIBRARY \
         -DOPENCV_PATH=$OPENCV_PATH \
         -DNCNN_PATH=$NCNN_PATH \
         -DNCNN_VULKAN=$NCNN_VULKAN ../../..
 fi
 
-cmake --build . -j4
-cmake --build . --target install
+cmake --build . --config Release
+cmake --install . --config Release
--- scripts/ios/ios_compile_batch.sh
+++ scripts/ios/ios_compile_batch.sh
@@ -8,11 +8,10 @@ NCNN_VULKAN=ON
 WITH_FULL_OPENCV=OFF
 INSTALL_EXAMPLES=OFF
 TOOLCHAIN_PATH="$(pwd)/toolchains"
-OPENMP_LIBRARY="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/lib/libomp.a"
 VULKAN_PATH="/Users/huang/Desktop/ncnn-20210525-full-source/vulkansdk-macos-1.2.162.0"
 NCNN_PATH="/Users/huang/Desktop/Cpp-VisionEigen/lib/ncnn-20210525-ios-vulkan"
 OPENCV_PATH="/Users/huang/Desktop/Cpp-VisionEigen/lib/opencv-mobile-4.5.1-ios"
 
 echo 'Build ios!'
-sh compile_ios.sh $TOOLCHAIN_PATH $VULKAN_PATH $NCNN_PATH $OPENCV_PATH $NCNN_VULKAN $WITH_FULL_OPENCV $INSTALL_EXAMPLES $OPENMP_LIBRARY $INSTALL_DIR
+sh compile_ios.sh $TOOLCHAIN_PATH $VULKAN_PATH $NCNN_PATH $OPENCV_PATH $NCNN_VULKAN $WITH_FULL_OPENCV $INSTALL_EXAMPLES $INSTALL_DIR
 rm -rf $BUILD_DIR
